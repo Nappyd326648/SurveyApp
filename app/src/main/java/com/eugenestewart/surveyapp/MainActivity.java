b789e4f860a02adac129a2ebd20d2c608f8eb679
Nappyd326648 <eugenecs326648@yahoo.com>
2016-09-07 15:29:10 -0500

    finished

--- app/src/main/java/com/eugenestewart/surveyapp/MainActivity.java
+++ app/src/main/java/com/eugenestewart/surveyapp/MainActivity.java
@@ -2,12 +2,59 @@ package com.eugenestewart.surveyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+//saves total votes
+    int myes;
+    int mno;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+// Assigns buttons to activity_main,xml
+        Button yes =(Button) findViewById(R.id.answerYes);
+        Button no =(Button) findViewById(R.id.answerNo);
+        final Button total =(Button)findViewById(R.id.total);
+        Button reset = (Button)findViewById(R.id.reset);
+//add listeners
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myes=myes+1;
+            }
+        });
+        no.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mno=mno+1;
+            }
+        });
+        total.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Yes total: "+ myes,Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this,"No total: "+ mno,Toast.LENGTH_LONG).show();
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mno=0;
+                myes=0;
+            }
+        });
+
     }
 }
